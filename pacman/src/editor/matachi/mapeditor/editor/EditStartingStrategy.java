ba362b2b6fdad2c672728c4b258d9a283717bd7e
Dillonwong12 <dillonwong12@gmail.com>
2023-05-26 07:25:23 +1000

    Update EditStartingStrategy to just return

--- pacman/src/editor/matachi/mapeditor/editor/EditStartingStrategy.java
+++ pacman/src/editor/matachi/mapeditor/editor/EditStartingStrategy.java
@@ -3,6 +3,6 @@ package src.editor.matachi.mapeditor.editor;
 public class EditStartingStrategy implements StartingStrategy{
     @Override
     public void startEditor(String[] argsEditor) {
-        Controller controller = Controller.getInstance();
+        return;
     }
 }
